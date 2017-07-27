052dcd113c45d2676e4a6c6cb73bacf219682440
iamzozo <iamzozo@gmail.com>
2017-07-27 14:50:49 +0200

    Remove unused imports

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Favorite;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 class FavoriteController extends Controller
 {
